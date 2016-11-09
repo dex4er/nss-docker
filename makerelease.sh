360be9c0bbfeec8f1978714bd616ecc8a84b2a84
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2016-11-09 23:04:44 +0100

    Script for preparing new release

---
+++ makerelease.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+name=`grep AC_INIT configure.ac | perl -pe '$_ = (/\[(.*?)\]/g)[0]'`
+version=`grep AC_INIT configure.ac | perl -pe '$_ = (/\[(.*?)\]/g)[1]'`
+
+tarball="$name-$version.tar.gz"
+
+test -f "$tarball" || exit 1
+
+cat > .git/gbp.conf << END
+[import-orig]
+dch = False
+interactive = False
+merge = False
+pristine-tar = False
+symlink-orig = False
+upstream-branch = release
+upstream-tag = %(version)s
+END
+
+set -x
+
+git fetch origin +release:release
+git tag -a -m "Release $version" master/$version
+gbp import-orig "$tarball"
